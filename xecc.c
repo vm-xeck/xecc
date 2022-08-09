fde60fd6b037380211541876b9d27663ed3d9198
vm-xeck <pika.mimi.8.09@gmail.com>
2022-08-10 01:30:27 +0900

    Fix typ.

--- xecc.c
+++ xecc.c
@@ -62,7 +62,7 @@ Token *new_token(TokenKind kind, Token *cur, char *str){
     Token *tok = calloc(1, sizeof(Token));
     tok->kind = kind;
     tok->str = str;
-    tok->next = tok;
+    cur->next = tok;
     return tok;
 }
 
